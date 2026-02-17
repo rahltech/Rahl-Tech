cda120651aea794f6decd811f568909b2c4c9ad5
rahltech <albertally265@gmail.com>
2026-02-17 04:29:25 +0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,17 +1,36 @@
-sock.ev.on("connection.update", async (update) => {
-    const { connection, lastDisconnect } = update;
+const makeWASocket = require("@whiskeysockets/baileys").default;
+const { useMultiFileAuthState, fetchLatestBaileysVersion } = require("@whiskeysockets/baileys");
+const fs = require("fs");
+const path = require("path");
+const { encodeBase44 } = require("../base44");
 
-    if (connection === "close") {
-        console.log("Connection closed. Reconnecting...");
-        await createSocket(sessionId);
-    }
+async function createSocket(sessionId) {
+    const sessionPath = path.join(__dirname, "../sessions", sessionId);
 
-    if (connection === "open") {
-        console.log("Rahlxmd Connected");
+    const { state, saveCreds } = await useMultiFileAuthState(sessionPath);
+    const { version } = await fetchLatestBaileysVersion();
 
-        const creds = fs.readFileSync(path.join(sessionPath, "creds.json"));
-        const base44Session = encodeBase44(creds);
+    const sock = makeWASocket({
+        version,
+        auth: state,
+    });
 
-        console.log("Base44 Session:", base44Session);
-    }
-});
+    sock.ev.on("creds.update", saveCreds);
+
+    sock.ev.on("connection.update", async (update) => {
+        const { connection } = update;
+
+        if (connection === "open") {
+            console.log("Rahlxmd Connected");
+
+            const creds = fs.readFileSync(path.join(sessionPath, "creds.json"));
+            const base44Session = encodeBase44(creds);
+
+            console.log("Base44 Session:", base44Session);
+        }
+    });
+
+    return sock;
+}
+
+module.exports = { createSocket };
