128947f876934f6bc68254e4c694220ab68f959c
rahltech <albertally265@gmail.com>
2026-02-17 03:40:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const config = require("./config");
-const pairRoute = require("./routes/pair.routes");
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(bodyParser.json());
-app.use(express.static("public"));
-
-app.use("/pair", pairRoute);
-
-app.listen(config.PORT, () => {
-    console.log(`🔥 Rahlxmd Web Server running on port ${config.PORT}`);
+app.listen(PORT, () => {
+    console.log(`🔥 Rahlxmd running on port ${PORT}`);
 });
