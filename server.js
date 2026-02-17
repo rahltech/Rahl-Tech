d2ed58c769b5bafb546044d35e6ac9cd2cc2bb32
rahltech <albertally265@gmail.com>
2026-02-17 03:52:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const pairRoute = require("./routes/pair.routes");
+
+const app = express();   
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+app.use("/pair", pairRoute);
+
+app.get("/", (req, res) => {
+    res.send("🔥 Rahlxmd is running");
+});
+
+
+if (!fs.existsSync("./sessions")) {
+    fs.mkdirSync("./sessions");
+}
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
